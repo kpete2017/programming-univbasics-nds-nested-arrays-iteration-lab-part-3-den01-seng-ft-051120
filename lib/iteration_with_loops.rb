ff2e167219f75997537f92f3029d88e04e360a28
Kyle Petersen <kpete2017@gmail.com>
2020-04-18 02:40:14 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,9 +6,10 @@ counter = 0
     while internal_counter < src[counter].length do
       if src[counter][internal_counter] == String
         new_string = new_string + " " + src[counter][internal_counter] 
-        internal_counter += 1
+      end
+      internal_counter += 1
     end
-    internal_counter += 1
+    counter += 1
   end
   new_string
 end
