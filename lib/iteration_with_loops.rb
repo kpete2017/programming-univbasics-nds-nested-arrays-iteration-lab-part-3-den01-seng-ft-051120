ac6ec60bff25437dcb1a8c0ca1649c0e91520c5d
Kyle Petersen <kpete2017@gmail.com>
2020-04-18 02:39:13 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,11 +1,11 @@
 def join_nested_strings(src)
-new_array = []
+new_string = ""
 counter = 0
   while counter < src.length do
     internal_counter = 0
     while internal_counter < src[counter].length do
       if src[counter][internal_counter] == String
-        new_array[counter][internal_counter] = src[counter][internal_counter] 
+        new_string = new_string + " " + src[counter][internal_counter] 
         internal_counter += 1
     end
     internal_counter += 1
